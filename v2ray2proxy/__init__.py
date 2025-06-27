a2358bb404caaffce83233d2a912bb4c9b3cef83
nichind <nichinddev@gmail.com>
2025-06-27 23:12:04 +0300

    Update readme; change version

--- v2ray2proxy/__init__.py
+++ v2ray2proxy/__init__.py
@@ -3,6 +3,6 @@ v2ray2proxy - Convert V2Ray configs to usable proxies for HTTP clients
 """
 from .base import V2RayCore, V2RayProxy, V2RayPool
 
-VERSION = "0.2"
+VERSION = "0.2.1"
 
 __all__ = ["V2RayCore", "V2RayProxy", "V2RayPool", "VERSION"]
